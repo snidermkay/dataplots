6ea91e70d23d19e0c77c8405e578af14d44bd74b
Kira <snidermk.sei@gmail.com>
2023-12-18 12:04:35 -0500

    calculating data automatically

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,11 +1,11 @@
 import  matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chat title and lavel axes.
 ax.set_title("Square Numbers", fontsize=24)

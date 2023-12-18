bee99aee47d9944082bfd18fee60591517e840e8
Kira <snidermk.sei@gmail.com>
2023-12-18 11:58:58 -0500

    plotting a single point

--- mpl_squares.py
+++ mpl_squares.py
@@ -5,7 +5,7 @@ squares = [1, 4 ,9, 16, 25]
 
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(2, 4, s=200)
 
 # Set chat title and lavel axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,6 +13,6 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Squre of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
